50d6c5d30620cb153442cae36900aa426752e86c
Emerson_Britto <emersonbritto987@gmail.com>
2021-07-25 02:39:36 -0300

    update files

--- adminTools/createJson.js
+++ adminTools/createJson.js
@@ -1,7 +1,22 @@
 const axios = require('axios');
 
+const setDuration = (music) => {
+	var fliter1 = music['contentDetails']['duration'].replace('PT', '');
+	filter2 = fliter1.replace(/M\dS/, ':00');
+	return filter2.replace(/M(?!\d)/, ':00').replace(/M/, ':').replace(/S/, '');
+}
+
+const getApiResult = async (listToSearch, resultNoFormat, key) => {
+	var ids = listToSearch.join('%2C')
+	var { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=${ids}&key=${key}`)
+    data.items.forEach(music => {
+        resultNoFormat.push(music);
+    })	
+}
+
 const getListFromYT = async (idsTotalResult, key) => {
 	var totalToSearch = idsTotalResult.length;
+	var resultNoFormat =[];
 	var musicTotalResult =[];
     var listToSearch =[];
     var alreadyResearched = 0;
@@ -14,22 +29,15 @@ const getListFromYT = async (idsTotalResult, key) => {
 		    if(listToSearch.length == 50){
 		    	totalToSearch = totalToSearch - listToSearch.length;
 
-				var ids = listToSearch.join('%2C')
-				var { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=${ids}&key=${key}`)
-			    data.items.forEach(music => {
-			        musicTotalResult.push(music);
-			    })
+                await getApiResult(listToSearch, resultNoFormat, key);
 
 				listToSearch =[]; 
 		    }
     	}
-		var ids = listToSearch.join('%2C')
-		var { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=${ids}&key=${key}`)
-	    data.items.forEach(music => {
-	        musicTotalResult.push(music);
-	    })
 
-	    musicTotalResult.forEach(music =>{
+        await getApiResult(listToSearch, resultNoFormat, key);
+
+	    resultNoFormat.forEach(music =>{
 	    	delete music['snippet']['description']
 	    	delete music['snippet']['localized']['description']
 	        delete music['kind']
@@ -38,9 +46,19 @@ const getListFromYT = async (idsTotalResult, key) => {
 	    	delete music['snippet']['thumbnails']['high']
 	    	delete music['snippet']['thumbnails']['standard']
 	    	delete music['snippet']['publishedAt']
+
+	    	var re = /.(\(|\[)(.)?\w+(.\w+)?(\)|\]|.)(-.|\/.)?(\w+.\w+)?(\]|\))?/;
+	    	music['snippet']['title'] = music['snippet']['title'].replace(re, '');
+
+	    	music['contentDetails']['duration'] = setDuration(music);
+
+	        let hasSomeEvenNumber = musicTotalResult.some(value => value.id === music.id);
+	        if (hasSomeEvenNumber === false){
+	            musicTotalResult.push(music);
+	        }
 	    })
-	    
-	    return musicTotalResult;	
+
+	    return musicTotalResult;
     }
 }
 
