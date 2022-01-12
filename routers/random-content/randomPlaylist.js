fffdf00af10dbfce4254d861b7228ab492401a4b
Emerson_Britto <emersonbritto987@gmail.com>
2022-01-12 06:04:07 -0300

    some adjust - route randomPlaylist

--- routers/random-content/randomPlaylist.js
+++ routers/random-content/randomPlaylist.js
@@ -1,15 +1,20 @@
 const { request } = require('../../external/api');
+const faker = require('faker');
 
 
-const randomPlaylists = async({ totalList }) => {
+const randomPlaylists = async({ totalList, category='all' }) => {
     
     let playlists = {
         request: 'random-list',
+        requestId: faker.datatype.uuid(),
         items: [],
         length: 0
     };
 
-    let lists = await request('allPlaylist', `?random=1&maxResult=${totalList}`);
+    let lists = await request(
+        'allPlaylist',
+        `?random=1&categoryInput=${category}&maxResult=${totalList}`
+    );
     playlists.items = lists.items;
 
     playlists.length = lists.itemsLength;
